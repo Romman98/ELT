32ef8f723094faa1478731db5ce5aebe0fbc5b35
Ahmad Romman - Desktop <rommaneus@hotmail.com>
2026-02-16 21:29:13 +0300

    Add get_video_ids

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
-
+maxResults=50
 def get_playlist_id():
 
     try:
@@ -23,8 +23,42 @@ def get_playlist_id():
         return channel_playlistsId
     except requests.exceptions.RequestException as e :
         raise e
+
+def get_video_ids(playlistId):
+    video_ids = []
     
+    page_token = None
+    
+    base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    
+    try:
+        while True:
+            url = base_url
+            
+            if page_token:
+                url += f"&pageToken={page_token}" 
+                
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+        
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            page_token = data.get('nextPageToken')
+            
+            if not page_token:
+                break
+        return video_ids
+    
+    except requests.exceptions.RequestException as e :
+        raise e
+        
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    print(get_video_ids(playlistId))
+    
 else:
     print("get_playlist_id wont be executed")
